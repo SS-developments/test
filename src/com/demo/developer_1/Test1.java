037d7b2aa21af5416728d66c9dd5eab13a133be3
SS-developments <swapnilawad2000@gmail.com>
2023-11-02 18:37:47 +0530

    commit from developer1

---
+++ src/com/demo/developer_1/Test1.java
@@ -0,0 +1,14 @@
+package com.demo.developer_1;
+
+public class Test1 {
+
+	
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("hello from developer 1 ");
+		
+		
+	}
+}
---
+++ src/com/demo/developer_1/Test2.java
@@ -0,0 +1,8 @@
+package com.demo.developer_1;
+
+public class Test2 {
+	public static void main(String[] args) {
+		System.out.println(" hello from developer 1");
+	}
+
+}
