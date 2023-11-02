55c4fbb90bb03050f5d199b5ed5e90add0388f17
SS-developments <swapnilawad2000@gmail.com>
2023-11-02 18:51:33 +0530

    dev2 make chnages

--- src/com/demo/dev2/test1.java
+++ src/com/demo/dev2/test1.java
@@ -3,6 +3,7 @@ package com.demo.dev2;
 public class test1 {
 	public static void main(String[] args) {
 		System.out.println(" hello from dev2");
+		System.out.println(" first pushed to master but not understand now pushing to dev2");
 	}
 
 }
